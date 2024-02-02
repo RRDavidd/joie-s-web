03e554919278f7f558133beb6136dfb2f97b3d58
Jala <raphaelruizd@gmail.com>
2024-02-02 18:53:48 +1100

    Fix html layout of header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Header() {
   return (
     <>
-      <figure className="relative w-100 h-[950px]">
+      <figure className="relative w-100 h-[950px] shadow-inner">
         <Image
           src={"/@/../nana.jpg"}
           fill
@@ -11,11 +11,13 @@ export default function Header() {
           className="object-cover opacity-30"
           priority
         />
-        <h1 className="font-mono text-default md:text-9xl sm:text-6xl text-5xl whitespace-nowrap absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 text-shadow-xl">
-          JOIEBELLS {"<3"}
-        </h1>
+        <div className="absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 ">
+          <h1 className="font-mono text-default md:text-9xl sm:text-6xl text-5xl whitespace-nowrap text-shadow-xl">
+            JOIEBELLS {"<3"}
+          </h1>
+          <p className="text-center">Hi beb happi valentines day :D</p>
+        </div>
       </figure>
-      <h1>Testing</h1>
     </>
   );
 }
